8dc4579fd8d446a38b10141b9087499cbf4a4cb4
xDwi54 <60254669+xDwi54@users.noreply.github.com>
2020-12-07 01:27:18 +0200

    Add files via upload

---
+++ Oma projekti 1/Form2.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Oma_projekti_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double txt1 = Convert.ToDouble(textBox1.Text);
+            double txt2 = Convert.ToDouble(textBox2.Text);
+
+            double sum = txt1 + txt2;
+
+            textBox3.Text = sum.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            double txt1 = Convert.ToDouble(textBox1.Text);
+            double txt2 = Convert.ToDouble(textBox2.Text);
+
+            double sum = txt1 - txt2;
+
+            textBox3.Text = sum.ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            double txt1 = Convert.ToDouble(textBox1.Text);
+            double txt2 = Convert.ToDouble(textBox2.Text);
+
+            double sum = txt1 / txt2;
+
+            textBox3.Text = sum.ToString();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            double txt1 = Convert.ToDouble(textBox1.Text);
+            double txt2 = Convert.ToDouble(textBox2.Text);
+
+            double sum = txt1 * txt2;
+
+            textBox3.Text = sum.ToString();
+        }
+    }
+}
